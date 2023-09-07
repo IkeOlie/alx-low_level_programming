ebd3b68ffb6a20003d0e93680aa663ccfa0436bc
IkeOlie <ikechukwuolie@yahoo.com>
2023-09-07 14:49:12 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+/**
+ * main - entry point
+ *
+ * Hello C
+ *
+ * return: always 0(success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

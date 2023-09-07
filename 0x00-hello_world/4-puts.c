a2c234747555ec25a585860594adb23b3e24a3e8
IkeOlie <ikechukwuolie@yahoo.com>
2023-09-07 15:34:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <studio.h>
 /**
- * main - entry point
+ * main . entry point
  *
  * Hello C
  *
- * return: always 0(success)
+ * return; always 0(success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle")
+		return(0);
 }

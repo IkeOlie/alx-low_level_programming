b655194b51ae60933ab254c71a494eafedc967ff
IkeOlie <ikechukwuolie@yahoo.com>
2023-09-08 16:20:39 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * mai - prints the alphabets
+ * Return: always 0(success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}

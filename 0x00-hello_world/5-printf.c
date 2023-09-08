6d0dfb4c7be077a5f1f23348acfbd5bbcc0f4c06
IkeOlie <ikechukwuolie@yahoo.com>
2023-09-07 17:08:57 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#inlude <stdio>
+/**
+ * main - entry point
+ *
+ * Hello C
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome ia a piece of art, \n");
+	return(0);
+}

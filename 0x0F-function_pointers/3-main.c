332464dce15c484d01145f0e9e83f48af0312e05
IkeOlie <ikechukwuolie@yahoo.com>
2023-10-11 19:11:19 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	oprt = get_op_func(argv[2]);
 	if (!oprt)
 	{
-		printf("Erro\n");
+		printf("Error\n");
 		exit(99);
 	}
 	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
